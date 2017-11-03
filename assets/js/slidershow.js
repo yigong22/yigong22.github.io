2f8f700065d8d41ea3ebcd3c6b7d5313f8e423be
KUNG E <hellchant22@me.com>
2017-11-04 02:27:35 +1100

    123
    123

---
+++ assets/js/slidershow.js
@@ -0,0 +1,37 @@
+
+
+var currentIndex = 0,
+  items = $('.slideshow-container'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.slideshow-container ').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 1000);
+
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
