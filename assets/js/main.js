f5e0ba6b421d85832263900c3f67d30652d1f059
KUNG E <hellchant22@me.com>
2017-11-05 18:51:05 +1100

    123
    123

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,30 @@ $(document).ready(function() {
 
 // Add jQuery here
 
+$('.stamp1').click(function(event) {
+  $('.stamp1').addClass('displayhover');
+  $('.stamp2').removeClass('displayhover');
+  $('.stamp3').removeClass('displayhover');
+});
 
 
+$('.stamp2').click(function(event) {
+  $('.stamp2').addClass('displayhover');
+  $('.stamp1').removeClass('displayhover');
+  $('.stamp3').removeClass('displayhover');
+});
 
 
+$('.stamp3').click(function(event) {
+  $('.stamp3').addClass('displayhover');
+  $('.stamp2').removeClass('displayhover');
+  $('.stamp1').removeClass('displayhover');
+});
 
 
-
-
-
-
+$('.lightbox').click(function(event){
+  $(this).toggleClass('active');
+});
 
 
 
